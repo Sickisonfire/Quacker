4d963197492e2d83b989059324de1664b06f4013
Marcel <firebird10@gmx.de>
2021-07-26 16:42:13 +0200

    misc

--- src/App.tsx
+++ src/App.tsx
@@ -3,37 +3,44 @@ import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import React from 'react';
 import SideMenu from './components/SideMenu/SideMenu';
-import { Container } from '@chakra-ui/react';
+import { Box, Container, Flex, Spacer } from '@chakra-ui/react';
 import { Feed } from './components/Feed/Feed';
 import { Searchbar } from './components/Searchbar/Searchbar';
+import { SideContent } from './components/SideContent/SideContent';
 
 const App = () => {
   return (
     <Router>
       <Container maxW={['full', 'md', '7xl']}>
-        {/*<SideMenu />
-         */}
-        <Route path="/login">
-          <LoginPage />
-        </Route>
-        <Route path="/signup">
-          <SignupPage />
-        </Route>
-        <Route path="/home">
-          <Feed />
-        </Route>
-        <Route path="/messages">
-          <div>messages</div>
-        </Route>
-        <Route path="/settings">
-          <div>settings</div>
-        </Route>
-        <Route path="/search">
-          <Searchbar />
-        </Route>
-        <Route path="/user/:username" exact>
-          <div>user</div>
-        </Route>
+        <Flex>
+          <Spacer />
+          <SideMenu />
+          <Box maxW="xl">
+            <Route path="/login">
+              <LoginPage />
+            </Route>
+            <Route path="/signup">
+              <SignupPage />
+            </Route>
+            <Route path="/home">
+              <Feed />
+            </Route>
+            <Route path="/messages">
+              <div>messages</div>
+            </Route>
+            <Route path="/settings">
+              <div>settings</div>
+            </Route>
+            <Route path="/search">
+              <Searchbar />
+            </Route>
+            <Route path="/user/:username" exact>
+              <div>user</div>
+            </Route>
+          </Box>
+          <SideContent />
+          <Spacer />
+        </Flex>
       </Container>
     </Router>
   );
--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -1,31 +1,70 @@
 import React from 'react';
-import { Button, Stack, IconButton } from '@chakra-ui/react';
+import {
+  Button,
+  Stack,
+  IconButton,
+  useBreakpointValue,
+  Box,
+} from '@chakra-ui/react';
 import { Logo } from '../Logo/Logo';
 import { AiFillHome, AiFillMessage } from 'react-icons/ai';
+import { GiDuckPalm } from 'react-icons/gi';
 import { RiUserFill } from 'react-icons/ri';
 import { HiDotsVertical } from 'react-icons/hi';
 import { MenuButton, DropdownButton } from './MenuButton';
 import { Link } from 'react-router-dom';
 
 const SideMenu = () => {
+  const isXL = useBreakpointValue({ xl: true });
   return (
-    <Stack direction="column" maxW="2xl" spacing="4">
-      <Link to="/home">
-        <IconButton icon={<Logo />} aria-label="home" variant="ghost" />
-      </Link>
-      <MenuButton to="/home" icon={<AiFillHome />}>
-        Home
-      </MenuButton>
-      <MenuButton to="/messages" icon={<AiFillMessage />}>
-        Messages
-      </MenuButton>
-      <MenuButton to="/profile" icon={<RiUserFill />}>
-        Profile
-      </MenuButton>
-      <DropdownButton icon={<HiDotsVertical />} />
-
-      <Button px="20">Quack</Button>
-    </Stack>
+    <Box>
+      <Stack
+        pos="sticky"
+        zIndex="sticky"
+        top="0"
+        direction="column"
+        maxW="2xl"
+        spacing="8"
+        pr="10"
+        py="2"
+      >
+        <Link to="/home">
+          <IconButton icon={<Logo />} aria-label="home" variant="ghost" />
+        </Link>
+        <MenuButton to="/home" icon={<AiFillHome size="28" />} label="Home">
+          Home
+        </MenuButton>
+        <MenuButton
+          to="/messages"
+          icon={<AiFillMessage size="28" />}
+          label="Messages"
+        >
+          Messages
+        </MenuButton>
+        <MenuButton
+          to="/profile"
+          icon={<RiUserFill size="28" />}
+          label="Profile"
+        >
+          Profile
+        </MenuButton>
+        <DropdownButton icon={<HiDotsVertical size="28" />} label="Settings">
+          More
+        </DropdownButton>
+        {isXL ? (
+          <Button px="20" colorScheme="green">
+            Quack
+          </Button>
+        ) : (
+          <IconButton
+            aria-label="Compose Quack"
+            icon={<GiDuckPalm size="28" />}
+            fontSize="xl"
+            colorScheme="green"
+          />
+        )}
+      </Stack>
+    </Box>
   );
 };
 
