a4c98ccec6b33460de77be4df4554739afa1703b
Marcel <firebird10@gmx.de>
2021-07-27 20:11:14 +0200

    added Switch for routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import React from 'react';
-import SideMenu from './components/SideMenu/SideMenu';
 import { Box, Container, Flex, Spacer } from '@chakra-ui/react';
 import { Feed } from './components/Feed/Feed';
 import { Searchbar } from './components/Searchbar/Searchbar';
 import { SideContent } from './components/SideContent/SideContent';
+import SideMenu from './components/SideMenu/SideMenu';
 
 const App = () => {
   return (
@@ -16,27 +16,29 @@ const App = () => {
           <Spacer />
           <SideMenu />
           <Box maxW="xl">
-            <Route path="/login">
-              <LoginPage />
-            </Route>
-            <Route path="/signup">
-              <SignupPage />
-            </Route>
-            <Route path="/home">
-              <Feed />
-            </Route>
-            <Route path="/messages">
-              <div>messages</div>
-            </Route>
-            <Route path="/settings">
-              <div>settings</div>
-            </Route>
-            <Route path="/search">
-              <Searchbar />
-            </Route>
-            <Route path="/user/:username" exact>
-              <div>user</div>
-            </Route>
+            <Switch>
+              <Route path="/login">
+                <LoginPage />
+              </Route>
+              <Route path="/signup">
+                <SignupPage />
+              </Route>
+              <Route path="/home" exact>
+                <Feed />
+              </Route>
+              <Route path="/messages">
+                <div>messages</div>
+              </Route>
+              <Route path="/settings">
+                <div>settings</div>
+              </Route>
+              <Route path="/search">
+                <Searchbar />
+              </Route>
+              <Route path="/:username">
+                <div>user</div>
+              </Route>
+            </Switch>
           </Box>
           <SideContent />
           <Spacer />
